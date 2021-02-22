a63dd468d1eec96d8404e602a0a0789b8b520f8b
José Guilherme <jguilhermebp@live.com>
2021-02-22 20:07:23 -0300

    App Center Setup

--- Swift-App/ViewController.swift
+++ Swift-App/ViewController.swift
@@ -6,17 +6,12 @@
 //
 
 import UIKit
-import AppCenter
-import AppCenterCrashes
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        AppCenter.start(withAppSecret: "726a7abd-f3ce-4064-8d08-9de508aa3cdb", services:[
-            Crashes.self
-        ])
     }
 }
 
